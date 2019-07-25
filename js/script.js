54a7120284acba6cdde559eeb6a8597f86dcf5bf
Luganskyi Anton <42344374+anton1408@users.noreply.github.com>
2019-07-25 23:19:38 +0300

    remowed bug with calc v.2

--- js/script.js
+++ js/script.js
@@ -259,7 +259,7 @@ window.addEventListener('DOMContentLoaded', function() {
         personsSum = +this.value;
         total = (daysSum + personsSum)*4000;
 
-        if(restDays.value == '' || persons.value == '' || restDays.value == 0) {
+        if(restDays.value == '' || persons.value == '' || persons.value == 0) {
             totalValue.innerHTML = 0;
         } else {
             totalValue.innerHTML = total * place.options[place.selectedIndex].value;
@@ -270,7 +270,7 @@ window.addEventListener('DOMContentLoaded', function() {
         daysSum = +this.value;
         total = (daysSum + personsSum)*4000;
 
-        if(persons.value == '' || restDays.value == '' || persons.value == 0) {
+        if(persons.value == '' || restDays.value == '' || restDays.value == 0) {
             totalValue.innerHTML = 0;
         } else {
             totalValue.innerHTML = total * place.options[place.selectedIndex].value;
@@ -278,7 +278,7 @@ window.addEventListener('DOMContentLoaded', function() {
     });
 
     place.addEventListener('change', function() {
-        if(restDays.value == '' || persons.value == '') {
+        if(restDays.value == '' || persons.value == '' || persons.value == 0 || restDays.value == 0) {
             totalValue.innerHTML = 0;
         } else {
             let a = total;
